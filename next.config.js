01ef75f2b1ba8a55ac2e9974f6e406c0bb7bdbf1
Samuelsonvs <m.sametatali@gmail.com>
2021-07-06 12:28:19 +0300

    profile picture host address added in next.config file

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const withMDX = require("@next/mdx")();
 module.exports = withMDX({
   pageExtensions: ["js", "mdx"],
   images: {
-    domains: ["lh3.googleusercontent.com"],
-    hostname: ["avatars.githubusercontent.com"],
+    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
   },
 });
